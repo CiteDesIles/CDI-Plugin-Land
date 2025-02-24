f1bccc4d09b981943c001ab2131065ab8feb191a
Azrotho <contact@azrotho.fr>
2025-02-24 22:12:24 +0100

    added toolbroke in db

--- src/main/java/fr/citedesiles/cdiland/listener/OnToolBrokeListener.java
+++ src/main/java/fr/citedesiles/cdiland/listener/OnToolBrokeListener.java
@@ -1,5 +1,10 @@
 package fr.citedesiles.cdiland.listener;
 
+import fr.citedesiles.cdiland.CDILandPlugin;
+import fr.citedesiles.cdiland.objectif.CDIObjectif;
+import fr.citedesiles.cdiland.objects.CDIPlayer;
+import fr.citedesiles.cdiland.objects.CDITeam;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerItemBreakEvent;
@@ -7,6 +12,18 @@ import org.bukkit.event.player.PlayerItemBreakEvent;
 public class OnToolBrokeListener implements Listener {
     @EventHandler
     public void on(PlayerItemBreakEvent event) {
-        
+        Player player = event.getPlayer();
+        CDIPlayer cdiPlayer = CDILandPlugin.instance().playerManager().get(player);
+        if(cdiPlayer == null) {
+            return;
+        }
+        CDITeam cdiTeam = CDILandPlugin.instance().teamManager().getTeam(cdiPlayer.getTeam());
+        if(cdiTeam == null) {
+            return;
+        }
+        CDIObjectif objectif = CDILandPlugin.instance().objectifManager().get(cdiTeam, "toolbroken");
+        if(objectif != null) {
+            objectif.setValue(objectif.getValue() + 1);
+        }
     }
 }
