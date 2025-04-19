1dda31ac359f4eae785a1a151d49486737c0f8a4
Azrotho <contact@azrotho.fr>
2025-04-19 15:01:21 +0200

    plop

--- src/main/java/fr/citedesiles/cdiland/listener/OnPlayerDamage.java
+++ src/main/java/fr/citedesiles/cdiland/listener/OnPlayerDamage.java
@@ -2,11 +2,13 @@ package fr.citedesiles.cdiland.listener;
 
 import org.bukkit.GameMode;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 
 public class OnPlayerDamage implements Listener {
+    @EventHandler
     public void onPlayerDamage(EntityDamageEvent event) {
         if(event.getEntity() instanceof Player player) {
             if(player.getGameMode().equals(GameMode.ADVENTURE)) {
